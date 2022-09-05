2fe678738f781a1467a453719c3f16c8a4a86d0c
Bunyameen <bunyameen@gmail.com>
2022-09-05 10:56:41 +0100

    tours project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react'
+import {toursData} from './toursData'
 import './App.css';
+import { Loading } from './Components/Loading';
 
 function App() {
+  const [isLoading, setIsloading] = useState(true)
+  const [tours, setTours] = useState(toursData)
+  const [readMore, setReadMore] = useState(false)
+  
+
+  const handleRemove = (id) =>{
+
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+
+  }
+
+  useEffect(() =>{
+    setTimeout(() => {
+      setIsloading(false)
+      
+    },1500);
+  })
+
+  
+
+
+
+
+
+
+  if(isLoading){
+    return <div>
+    <Loading />
+
+    </div>
+    
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {tours.length > 0 ? <h1 className="tour-txt">Our Tours</h1> : <h1 style={{textAlign:'center'}}>No Tours left</h1>}
+      
+
+      {tours.length > 0 ? (
+        tours.map((tour) => {
+          const { id, img, title, price, about } = tour;
+
+          return (
+            <main className="main" key={id}>
+              <section className="article-txt">
+                <img src={img} alt="" />
+                <p className="title-para">
+                  <span className="title">{title}</span>
+                  <span className="price">{price}</span>
+                </p>
+
+                <p className="about">
+                  {readMore ? about : about.slice(0, 170)}
+                  <span onClick={() => setReadMore(!readMore)} className="showText">
+                    {readMore ? "show less" : "...show more"}
+                  </span>
+                </p>
+                <button
+                  className="btn"
+                  type="button"
+                  onClick={() => handleRemove(id)}
+                >
+                  Not Interested
+                </button>
+              </section>
+            </main>
+          );
+        })
+      ) : (
+        <div className='refresh-div'>
+          <button onClick={() => setTours(toursData)} className="refresh-btn">
+            refresh
+          </button>
+        </div>
+      )}
     </div>
   );
 }
